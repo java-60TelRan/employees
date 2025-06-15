3333d513e8fc8ad61372b595afaac59932ba1b8f
Yuragranovsky <yuragranovsky@gmail.com>
2025-06-15 21:41:46 +0300

    Building SalaryStatisticsPage component

--- src/pages/SalaryStatisticsPage.tsx
+++ src/pages/SalaryStatisticsPage.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Employee } from '../model/dto-types'
+import { AxiosError } from 'axios'
+import { useQuery } from '@tanstack/react-query'
+import apiClient from '../services/ApiClientJsonServer'
+import Statistics from '../components/Statistics'
 
 const SalaryStatisticsPage = () => {
+  const {data: employees} = useQuery<Employee[], AxiosError>({
+    queryKey: ["employees"],
+    queryFn: () => apiClient.getAll(),
+    staleTime: 3600_000
+  })
+
   return (
-    <div>SalaryStatisticsPage</div>
+    <Statistics numbers={employees?.map(e => e.salary) || []} interval={5000} label={'Salary'} ></Statistics>
   )
 }
 
