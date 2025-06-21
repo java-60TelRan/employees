e62d85b8aca5ada4e4108b999e8a9088c4347092
Yuragranovsky <yuragranovsky@gmail.com>
2025-06-21 13:48:45 +0300

    HW #45 solution

--- src/components/EmployeeForm.tsx
+++ src/components/EmployeeForm.tsx
@@ -9,10 +9,13 @@ import {
   Field,
   NativeSelect,
   Button,
+  Input,
 } from "@chakra-ui/react";
+import { getDateFromAge } from "../util/functions";
 interface Props {
   submitter: (empl: Employee) => void;
 }
+
 const EmployeeForm: FC<Props> = ({ submitter }) => {
   const {
     register,
@@ -20,21 +23,20 @@ const EmployeeForm: FC<Props> = ({ submitter }) => {
     formState: { errors },
   } = useForm<Employee>();
   return (
-    <Stack as="form" onSubmit={handleSubmit(data => submitter(data))}>
+    <Stack as="form" onSubmit={handleSubmit(data => submitter(data))} height="80vh"  justifyContent={"space-around"}>
       <SimpleGrid
         columns={{
           base: 1,
           sm: 2,
           md: 3,
-        }}
+        }} gap={10} marginLeft={"10vh"}
       >
         <Field.Root invalid={!!errors.department}>
           <Field.Label>Department</Field.Label>
-          <NativeSelect.Root size="sm">
+          <NativeSelect.Root size="sm" width="80%">
             <NativeSelect.Field
               placeholder="Select Department"
               {...register("department", { required: true })}
-              
             >
                 {employeesConfig.departments.map(d => <option key={d} value={d}>{d}</option>)}
             </NativeSelect.Field>
@@ -42,10 +44,27 @@ const EmployeeForm: FC<Props> = ({ submitter }) => {
           </NativeSelect.Root>
           <Field.ErrorText>Department must be selected</Field.ErrorText>
         </Field.Root>
+         <Field.Root invalid={!!errors.fullName} width="80%">
+          <Field.Label>Full name</Field.Label>
+          <Input {...register("fullName", {required: true})} size="sm" placeholder="enter full name"/>
+          <Field.ErrorText>Full Name is required</Field.ErrorText>
+        </Field.Root>     
+        <Field.Root invalid={!!errors.birthDate} width="80%">
+          <Field.Label>Birthdate</Field.Label>
+          <Input {...register("birthDate", {required:true})} type="date" min={getDateFromAge(employeesConfig.maxAge)}
+          max={getDateFromAge(employeesConfig.minAge)} size="sm"/>
+          <Field.ErrorText>Birthdate is required</Field.ErrorText>
+        </Field.Root>
+        <Field.Root invalid={!!errors.salary}width="80%">
+          <Field.Label>Salary</Field.Label>
+          <Input {...register("salary",{required: true, min: employeesConfig.minSalary,
+             max: employeesConfig.maxSalary})}type="number" size="sm" placeholder={`enetr salary [${employeesConfig.minSalary}-${employeesConfig.maxSalary}]`}/>
+          <Field.ErrorText>{`Salary must be in the range [${employeesConfig.minSalary}-${employeesConfig.maxSalary}]`}</Field.ErrorText>
+        </Field.Root>
       </SimpleGrid>
-      <HStack>
-        <Button type="submit">Save</Button>
-        <Button type="reset">Reset</Button>
+      <HStack justifyContent={'space-around'} >
+        <Button type="submit" colorPalette={"blue"} variant="subtle">Save</Button>
+        <Button type="reset" colorPalette={"blue"} variant="subtle">Reset</Button>
       </HStack>
     </Stack>
   );
